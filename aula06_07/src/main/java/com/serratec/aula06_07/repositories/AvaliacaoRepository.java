5428cb0859c9e5decef5a2ac438491b38d32c873
Mario Praun <mpraun.dev@gmail.com>
2026-05-15 18:52:28 -0300

    Delete aula06_07 directory

--- aula06_07/src/main/java/com/serratec/aula06_07/repositories/AvaliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.serratec.aula06_07.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.serratec.aula06_07.models.Avaliacao;
-
-public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long>{
-
-}
--- aula06_07/src/main/java/com/serratec/aula06_07/repositories/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.serratec.aula06_07.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.serratec.aula06_07.models.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {}
